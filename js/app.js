ca7a87402e0ff5372b5cae85e3989736d45671a5
senait16-creator <senaitrichmond16@gmail.com>
2026-06-25 14:35:48 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-console.log("The Brain is connected!");
-<script type="module" src="js/app.js"></script>
-// inside js/app.js
+// 1. All your IMPORTS go at the very top
 import { SUPABASE_URL } from './config.js';
 import { initSketchpad } from './game.js';
 
+// 2. Then, your logic
+console.log("The Brain is connected!");
+console.log("Supabase URL is:", SUPABASE_URL);
+
+// 3. Then, your initialization functions
+initSketchpad();
