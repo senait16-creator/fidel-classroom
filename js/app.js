8b1b7c5c3b635a923a20833314710cfdfb37632d
senait16-creator <senaitrichmond16@gmail.com>
2026-06-25 19:08:10 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 // 1. All your IMPORTS go at the very top
 import { SUPABASE_URL } from './config.js';
 import { initSketchpad } from './game.js';
+import { handleAuth } from './auth.js';
+
+// Now your app.js can "talk" to your functions
+window.handleAuth = handleAuth;
 
 // 2. Then, your logic
 console.log("The Brain is connected!");
