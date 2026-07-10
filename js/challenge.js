584237b6637d2d50892182ad0ab85c40b7cbfea0
senait16-creator <senaitrichmond16@gmail.com>
2026-07-10 16:39:43 -0400

    Update challenge.js

--- js/challenge.js
+++ js/challenge.js
@@ -959,11 +959,7 @@ function renderChallengeFamilyDetailGiantRow(fidelObj) {
 // -----------------------------------------------------------------------------
 
 async function recordStreakProgress(baseLetter, levelNumber, bestStreak, passed) {
-    const { error } = await _supabase.from('student_family_progress').upsert({
-        student_id: currentUser.id, base_letter: baseLetter,
-        level_number: levelNumber, best_streak: bestStreak, streak_passed: passed
-    }, { onConflict: 'student_id,base_letter' });
-    if (error) console.error("Failed to save streak progress:", error);
+    await saveStreakProgress(baseLetter, levelNumber, bestStreak, passed);
 }
 
 function launchChallengeStreakGame(fidelObj, levelNumber) {
